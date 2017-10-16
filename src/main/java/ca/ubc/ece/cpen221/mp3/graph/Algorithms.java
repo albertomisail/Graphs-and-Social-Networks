d3c56adaed3bb9b7708978c8ea7ea4b59a2724aa
Alberto Misail <ajmisailguber@gmail.com>
2017-10-16 16:06:32 -0700

    MP3 sub

--- src/main/java/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
+++ src/main/java/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
@@ -1,10 +1,15 @@
 package ca.ubc.ece.cpen221.mp3.graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import ca.ubc.ece.cpen221.mp3.staff.Graph;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class Algorithms {
-
+	
 	/**
 	 * *********************** Algorithms ****************************
 	 *
@@ -24,9 +29,29 @@ public class Algorithms {
 	 * @param b
 	 * @return
 	 */
-	public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
+	public static int shortestDistance(Graph graph, Vertex a, Vertex b) throws NotFoundException {
 		// TODO: Implement this method and others
-		return 0;
+		if (a.equals(b)) {
+			return 0;
+		} else {
+			return shortestDistance(graph, a, b, 1);
+		}
+	}
+
+	private static int shortestDistance(Graph graph, Vertex a, Vertex b, int k) throws NotFoundException {
+		List<Vertex> downstreamNeighbors = graph.getDownstreamNeighbors(a);
+		int result = -1;// ask
+		if (k >= graph.getVertices().size() || downstreamNeighbors.size()==0) {
+			throw new NotFoundException();
+		}
+		if (downstreamNeighbors.contains(b)) {
+			result = k;
+		} else {
+			for (int i = 0; i < downstreamNeighbors.size(); i++) {
+				result = shortestDistance(graph, downstreamNeighbors.get(i), b, k + 1);
+			}
+		}
+		return result;
 	}
 
 	/**
@@ -69,33 +94,100 @@ public class Algorithms {
 	/**
 	 * You should write the spec for this method
 	 */
-	 public static Vertex center(Graph graph) {
+	 public static Vertex center(Graph graph) throws NoCenterException{
 		 // TODO: Implement this method
-		 return null; // this should be changed
+		 Map<Vertex, Integer> eccentricities = new HashMap<Vertex, Integer>();
+		 for (int i = 0; i < graph.getVertices().size(); i++) {
+		 	Vertex v = graph.getVertices().get(i);
+			int eccentricity = calculateEccentricity(graph, v);
+			if(eccentricity!=0) {
+				eccentricities.put(v, eccentricity);
+			}
+		 }
+		 if (eccentricities.size() == 0) {
+				throw new NoCenterException();
+			}
+			int min_eccentricity = Integer.MAX_VALUE;
+			Vertex center = null;
+			for (Map.Entry<Vertex, Integer> entry : eccentricities.entrySet()) {
+				if (entry.getValue() <= min_eccentricity) {
+					min_eccentricity = entry.getValue();
+					center = entry.getKey();
+				}
+			}
+		 return center; // this should be changed
 	 }
+		 
+	public static int calculateEccentricity(Graph graph, Vertex v){
+		int eccentricity = 0;
+		for (int i = 0; i < graph.getVertices().size(); i++) {
+			Vertex w = graph.getVertices().get(i);
+			try {
+				int distance = shortestDistance(graph, v, w);
+				if (distance > eccentricity) {
+					eccentricity = distance;
+				}
+			}catch(NotFoundException e) {
+				
+			}
+		}
+		return eccentricity;
+	}
 
 	 /**
 	  * You should write the spec for this method
 		*/
-		public static int diameter(Graph graph) {
-			// TODO: Implement this method
-			return -1; // this should be changed
+	public static int diameter(Graph graph) throws InfiniteDiameterException{
+		// TODO: Implement this method
+		int diameter = 0;
+		for(int i=0; i<graph.getVertices().size(); i++) {
+			Vertex v = graph.getVertices().get(i);
+			for(int j=0; j<graph.getVertices().size(); j++) {
+				Vertex w = graph.getVertices().get(j);
+				try{
+					if(diameter<Algorithms.shortestDistance(graph, v, w)) {
+						diameter = Algorithms.shortestDistance(graph, v, w);
+					}
+				}catch(NotFoundException e) {
+					
+				}
+			}
 		}
+		if(diameter==0) {
+			throw new InfiniteDiameterException();
+		}
+		return diameter; // this should be changed
+	}
 
 	/**
 	 * You should write the spec for this method
 	 */
 	 public static List<Vertex> commonUpstreamVertices(Graph graph, Vertex a, Vertex b) {
-		 // TODO: Implement this method
- 		return null; // this should be changed
+		// TODO: Implement this method
+		List<Vertex> upstreamVerticesA = graph.getUpstreamNeighbors(a);
+		List<Vertex> upstreamVerticesB = graph.getUpstreamNeighbors(b);
+		return commonVertices(upstreamVerticesA, upstreamVerticesB); // this should be changed
 	 }
 
-	 /**
+	 
+	private static List<Vertex> commonVertices(List<Vertex> list1, List<Vertex> list2){
+		List<Vertex> result = new ArrayList<Vertex>();
+		for(int i=0; i<list1.size(); i++) {
+			Vertex v = list1.get(i);
+			if(list2.contains(v)) {
+				result.add(v);
+			}
+		}
+		return result;
+	}
+	/**
  	 * You should write the spec for this method
  	 */
  	 public static List<Vertex> commonDownstreamVertices(Graph graph, Vertex a, Vertex b) {
- 		 // TODO: Implement this method
-  		return null; // this should be changed
+ 		// TODO: Implement this method
+ 		List<Vertex> downstreamVerticesA = graph.getDownstreamNeighbors(a);
+		List<Vertex> downstreamVerticesB = graph.getDownstreamNeighbors(b);
+		return commonVertices(downstreamVerticesA, downstreamVerticesB);
  	 }
 
 }
--- src/main/java/ca/ubc/ece/cpen221/mp3/graph/MP3Test.java
+++ src/main/java/ca/ubc/ece/cpen221/mp3/graph/MP3Test.java
@@ -9,13 +9,72 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class MP3Test {
-	@Test(expected = InfiniteDiameterException.class)
-	public void test1() throws InfiniteDiameterException{
+	@Test
+	public void test1() throws NotFoundException{
 		Vertex v1 = new Vertex("v1");
+		Vertex v = new Vertex("v1");
+		Vertex v2 = new Vertex("v2");
+		Vertex v3 = new Vertex("v3");
+		Vertex v4 = new Vertex("v4");
 		Graph g1 = new AdjacencyListGraph();
-		Graph g2 = new AdjacencyMatrixGraph();
 		g1.addVertex(v1);
-		g2.addVertex(v1);
+		g1.addVertex(v);
+		g1.addVertex(v2);
+		g1.addVertex(v3);
+		g1.addVertex(v4);
+		g1.addEdge(v1, v2);
+		g1.addEdge(v2, v3);
+		g1.addEdge(v2, v4);
+		assertEquals(0, Algorithms.shortestDistance(g1, v1, v));
+		assertEquals(0, Algorithms.shortestDistance(g1, v1, v1));
+		assertEquals(1, Algorithms.shortestDistance(g1, v1, v2));
+		assertEquals(2, Algorithms.shortestDistance(g1, v1, v3));
+		assertEquals(2, Algorithms.shortestDistance(g1, v1, v4));
+	}
+	
+	@Test(expected = NotFoundException.class)
+	public void test2() throws NotFoundException{
+		Vertex v2 = new Vertex("v2");
+		Vertex v3 = new Vertex("v3");
+		Vertex v4 = new Vertex("v4");
+		Graph g1 = new AdjacencyListGraph();
+		g1.addVertex(v2);
+		g1.addVertex(v3);
+		g1.addVertex(v4);
+		g1.addEdge(v2, v3);
+		System.out.println(Algorithms.shortestDistance(g1, v2, v4));
+	}
+	
+	@Test(expected = InfiniteDiameterException.class)
+	public void test3() throws InfiniteDiameterException{
+		Vertex v1 = new Vertex("v1");
+		Graph g1 = new AdjacencyMatrixGraph();
+		g1.addVertex(v1);
 		Algorithms.diameter(g1);
 	}
+	
+	@Test(expected = InfiniteDiameterException.class)
+	public void test4() throws InfiniteDiameterException{
+		Graph g1 = new AdjacencyMatrixGraph();
+		Algorithms.diameter(g1);
+	}
+	
+	@Test
+	public void test5() throws InfiniteDiameterException, NoCenterException{
+		Graph g1 = new AdjacencyMatrixGraph();
+		Vertex v1 = new Vertex("v1");
+		Vertex v2 = new Vertex("v2");
+		Vertex v3 = new Vertex("v3");
+		g1.addVertex(v1);
+		g1.addVertex(v2);
+		g1.addVertex(v3);
+		g1.addEdge(v1, v2);
+		assertEquals(1,Algorithms.diameter(g1));
+		assertEquals(v1,Algorithms.center(g1));
+		g1.addEdge(v2, v3);
+		g1.addEdge(v3, v1);
+		assertEquals(2,Algorithms.diameter(g1));
+	}
+	
+	
 }
